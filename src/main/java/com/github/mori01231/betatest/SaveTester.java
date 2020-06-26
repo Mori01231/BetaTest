d5b59fd545f3f3508f0ba138e82846c99988187d
Mori01231 <mori01231mc@gmail.com>
2020-06-26 15:11:58 +0900

    [Update] SaveTester command also logs info.

--- src/main/java/com/github/mori01231/betatest/SaveTester.java
+++ src/main/java/com/github/mori01231/betatest/SaveTester.java
@@ -1,8 +1,14 @@
 package com.github.mori01231.betatest;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static org.bukkit.Bukkit.getLogger;
+import static org.bukkit.Bukkit.getPlayer;
 
 
 public class SaveTester implements CommandExecutor {
@@ -14,8 +20,29 @@ public class SaveTester implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        plugin.betatesters.add(args[0]);
-        plugin.betatesters.save();
+
+
+        //Store mcid of player
+        String mcid = String.valueOf(args[0]);
+
+        Player player = getPlayer(mcid);
+
+        //Store uuid of player
+        String uuid = String.valueOf(player.getUniqueId());
+
+
+
+        //Not saved as beta tester yet
+        if (!plugin.betatesters.contains(uuid)){
+            plugin.betatesters.add(uuid);
+            plugin.betatesters.save();
+            getLogger().info("Added beta tester " + mcid + " to list.");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスターとして登録されました。" ));
+        }
+        //Already saved as beta tester
+        else{
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスターとして登録済みです。" ));
+        }
 
         return true;
     }
