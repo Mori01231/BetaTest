4858ca5592aa80d19cef516fcca1dc62179d0659
Mori01231 <mori01231mc@gmail.com>
2020-06-26 14:32:44 +0900

    [Update] Loggers to confirm the plugin is working.

--- src/main/java/com/github/mori01231/betatest/PlayerJoinListener.java
+++ src/main/java/com/github/mori01231/betatest/PlayerJoinListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getServer;
 
 
@@ -19,27 +20,39 @@ public class PlayerJoinListener implements Listener {
         this.plugin = plugin;
     }
 
+    //Is the plugin in save mode or give mode?
     String mode = BetaTest.getInstance().getConfig().getString("Mode");
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        boolean firstJoin = !player.hasPlayedBefore();
+
+        //Store uuid of player
         String uuid = String.valueOf(player.getUniqueId());
 
-        Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "This player is on their first join: " + firstJoin + " UUID: " + uuid + " MODE: " + mode);
+        //Store mcid of player
+        String mcid = String.valueOf(player.getName());
+
+        //save mode
         if (mode.equalsIgnoreCase("save")){
+
+            //Not saved as beta tester yet
             if (!plugin.betatesters.contains(uuid)){
                 plugin.betatesters.add(uuid);
                 plugin.betatesters.save();
-                //Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "Saving BetaTester");
+                getLogger().info("Added beta tester" + mcid + " to list.");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスターとして登録されました。" ));
             }
-
-
-
+            //Already saved as beta tester
+            else{
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスターとして登録済みです。" ));
+            }
         }
 
+        //give mode
         else if (mode.equalsIgnoreCase("give")){
+
+            //Not given beta tester items yet
             if (plugin.betatesters.contains(uuid)){
                 //give mythicmobs items
                 for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MM")) {
@@ -53,13 +66,14 @@ public class PlayerJoinListener implements Listener {
 
                 plugin.betatesters.remove(uuid);
                 plugin.betatesters.save();
-                //Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "Saving BetaTester");
+                getLogger().info("Removed beta tester" + mcid + " from list.");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスター特典を配布しました。" ));
+            }
+            else{
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスター特典は配布済みです。" ));
             }
         }
-        /*if(firstJoin){
 
-
-        }*/
     }
 
 }
