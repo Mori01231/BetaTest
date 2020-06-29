f3fd85e4cc5045ed06d3f47065060fb6a61f1cf1
Mori01231 <mori01231mc@gmail.com>
2020-06-29 23:55:24 +0900

    [Add] additional logging for give command

--- src/main/java/com/github/mori01231/betatest/GiveTester.java
+++ src/main/java/com/github/mori01231/betatest/GiveTester.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getServer;
 
 public class GiveTester implements CommandExecutor {
@@ -14,11 +15,13 @@ public class GiveTester implements CommandExecutor {
         //give mythicmobs items
         for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MM")) {
             getServer().dispatchCommand(getServer().getConsoleSender(), "mm i give " + args[0] + " " + line);
+            getLogger().info("Gave beta tester " + args[0] + " mythicmobs item " + line);
         }
 
         //give minecraft items
         for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MC")) {
             getServer().dispatchCommand(getServer().getConsoleSender(), "minecraft:give " + args[0] + " " + line);
+            getLogger().info("Gave beta tester " + args[0] + " minecraft item " + line);
         }
 
         return true;
