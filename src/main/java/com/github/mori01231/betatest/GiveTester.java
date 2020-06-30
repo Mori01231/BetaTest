4d7a151a4f6c6b27279edf91f9dd1b8518aab4ff
Mori01231 <mori01231mc@gmail.com>
2020-06-30 23:54:07 +0900

    [Add] A significant amount of logging.

--- src/main/java/com/github/mori01231/betatest/GiveTester.java
+++ src/main/java/com/github/mori01231/betatest/GiveTester.java
@@ -1,27 +1,53 @@
 package com.github.mori01231.betatest;
 
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-import static org.bukkit.Bukkit.getLogger;
-import static org.bukkit.Bukkit.getServer;
+import static org.bukkit.Bukkit.*;
 
 public class GiveTester implements CommandExecutor {
+
+    private BetaTest plugin;
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        //give mythicmobs items
-        for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MM")) {
-            getServer().dispatchCommand(getServer().getConsoleSender(), "mm i give " + args[0] + " " + line);
-            getLogger().info("Gave beta tester " + args[0] + " mythicmobs item " + line);
-        }
+        getLogger().info("Using GiveTester command.");
 
-        //give minecraft items
-        for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MC")) {
-            getServer().dispatchCommand(getServer().getConsoleSender(), "minecraft:give " + args[0] + " " + line);
-            getLogger().info("Gave beta tester " + args[0] + " minecraft item " + line);
+        //Store mcid of player
+        String mcid = String.valueOf(args[0]);
+
+        Player player = getPlayer(mcid);
+
+        //Store uuid of player
+        String uuid = String.valueOf(player.getUniqueId());
+
+        if (plugin.betatesters.contains(uuid)){
+            //give mythicmobs items
+            for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MM")) {
+                getServer().dispatchCommand(getServer().getConsoleSender(), "mm i give " + player.getName() + " " + line);
+                getLogger().info("Gave beta tester " + mcid + " mythicmobs item " + line);
+            }
+
+            //give minecraft items
+            for (String line : BetaTest.getInstance().getConfig().getStringList("GiveTester.MC")) {
+                getServer().dispatchCommand(getServer().getConsoleSender(), "minecraft:give " + player.getName() + " " + line);
+                getLogger().info("Gave beta tester " + mcid + " minecraft item " + line);
+            }
+
+            plugin.betatesters.remove(uuid);
+            plugin.betatesters.save();
+            getLogger().info("Removed beta tester " + mcid + " from list.");
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスター特典を受け取りました。" ));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスター特典を配布しました。" ));
+        }
+        else{
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスター特典は受け取り済みです。" ));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b&lベータテスター特典は配布済みです。" ));
         }
 
         return true;
